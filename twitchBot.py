8a59ce926eaa955a4c701336f402ad9adba09058
xjeroen <jeroen.c.bongers@gmail.com>
2014-04-10 19:18:33 +0200

    Added more functions

--- twitchBot.py
+++ twitchBot.py
@@ -15,18 +15,26 @@ class IRCBot():
     def printInfo(self):
         print("Server: %s\nChannel: %s\nPort: %s\n" % (self.BOT_IRC_SERVER, self.BOT_IRC_CHANNEL, self.BOT_IRC_PORT))
 
-    def connect(self):
-        irc = socket.socket()
-        irc.connect((self.BOT_IRC_SERVER, self.BOT_IRC_PORT))
-        irc.recv(4096)
-        irc.send(bytes('NICK ' + self.BOT_NICKNAME + '\r\n', 'utf-8'))
-        irc.send(bytes('USER jbBot jbBot jbBot : jb IRC\r\n', 'utf-8'))
-        irc.send(bytes('JOIN ' + self.BOT_IRC_CHANNEL + '\r\n', 'utf-8'))
-        while 1:
-            line = irc.recv(4096)
-            print(line)
+    def connect(self):      # Connecting to the given IRC, afterwards it starts the lineParser.
+        self.irc = socket.socket()
+        self.irc.connect((self.BOT_IRC_SERVER, self.BOT_IRC_PORT))
+        self.irc.recv(4096)
+        self.irc.send(bytes('NICK ' + self.BOT_NICKNAME + '\r\n', 'utf-8'))
+        self.pingChecker(self.irc.recv(4096))       # Ping will sometimes be sent before motd, after joining
+        self.irc.send(bytes('USER jbBot jbBot jbBot : jb IRC\r\n', 'utf-8'))
+        self.pingChecker(self.irc.recv(4096))       # Sometimes after NICK, sometimes after USER.
+        self.irc.send(bytes('JOIN ' + self.BOT_IRC_CHANNEL + '\r\n', 'utf-8'))
+        self.lineParser()
 
-    def messagechecker(self,msgLine):
+
+    def lineParser(self):
+        while 1:
+            line = self.irc.recv(4096)
+            self.pingChecker(line)
+            if line.find(bytes('PRIVMSG', 'utf-8')) != -1 or line.find(bytes('NOTICE', 'utf-8')) != -1:
+                self.messagechecker(line)
+
+    def messagechecker(self, msgLine):
         completeLine = str(msgLine[1:]).split(':', 1)
         info = completeLine[0].split()
         message = completeLine[1].split("\\r")[0]
@@ -36,30 +44,27 @@ class IRCBot():
         print("\nMessage-->" + str(message))
         print("Sender-->" + str(sender) + "\n")
 
-    def lineParser(self, chat):
-        while 1:
-            line = chat.recv(4096)
-            #print(line)
-            if line.find(bytes('PRIVMSG', 'utf-8')) != -1 or line.find(bytes('NOTICE', 'utf-8')) != -1:
-                self.messagechecker(line)
-
+    def pingChecker(self, pingLine):
+        if pingLine.find(bytes('PING', 'utf-8')) != -1:
+            pingLine = pingLine.rstrip().split()
+            if pingLine[0] == bytes("PING", 'utf-8'):
+                self.irc.send(bytes("PONG ", 'utf-8') + pingLine[1] + bytes("\r\n", 'utf-8'))
 
 class TwitchBot(IRCBot):
     BOT_PASSWORD = "password"
 
     def connect(self):
-        irc = socket.socket()
-        irc.connect((self.BOT_IRC_SERVER, self.BOT_IRC_PORT))
-        irc.send(bytes('PASS ' + self.BOT_PASSWORD + '\r\n', 'utf-8'))
-        irc.send(bytes('USER jbBot jbBot jbBot : jb IRC\r\n', 'utf-8'))
-        irc.send(bytes('NICK ' + self.BOT_NICKNAME + '\r\n', 'utf-8'))
-        irc.send(bytes('JOIN ' + self.BOT_IRC_CHANNEL + '\r\n', 'utf-8'))
-        self.lineParser(irc)
+        self.irc = socket.socket()
+        self.irc.connect((self.BOT_IRC_SERVER, self.BOT_IRC_PORT))
+        self.irc.send(bytes('PASS ' + self.BOT_PASSWORD + '\r\n', 'utf-8'))
+        self.irc.send(bytes('USER jbBot jbBot jbBot : jb IRC\r\n', 'utf-8'))
+        self.irc.send(bytes('NICK ' + self.BOT_NICKNAME + '\r\n', 'utf-8'))
+        self.irc.send(bytes('JOIN ' + self.BOT_IRC_CHANNEL + '\r\n', 'utf-8'))
+        self.lineParser()
 
 
 #testBot = IRCBot("irc.geekshed.net", "#bottesting", 6667)
-#testBot.printInfo()
 #testBot.connect()
 
-twitchBot = TwitchBot("irc.twitch.tv", "#kungentv", 6667)
+twitchBot = TwitchBot("irc.twitch.tv", "#j_macc", 6667)
 twitchBot.connect()
