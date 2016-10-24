d5192b02355fa5e919f64b22b0be9f00a4ce2e5b
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2016-10-24 15:08:36 +0400

    removed all ,'utf-8' from file

--- IRCbot.py
+++ IRCbot.py
@@ -11,10 +11,10 @@ BOT_PASSWORD = "Password"
 
 
 def pingChecker(pingLine):
-    if pingLine.find(bytes('PING', 'utf-8')) != -1:
+    if pingLine.find(bytes('PING'  )) != -1:
         pingLine = pingLine.rstrip().split()
-        if pingLine[0] == bytes("PING", 'utf-8'):
-            irc.send(bytes("PONG ", 'utf-8') + pingLine[1] + bytes("\r\n", 'utf-8'))
+        if pingLine[0] == bytes("PING"  ):
+            irc.send(bytes("PONG "  ) + pingLine[1] + bytes("\r\n"  ))
 
 
 def messagechecker(msgLine):
@@ -28,31 +28,31 @@ def messagechecker(msgLine):
     print("Sender-->" + str(sender) + "\n")
 
     if message.lower() == "hi":
-        irc.send(bytes('PRIVMSG ' + BOT_IRC_CHANNEL + ' :Hi there, ' + str(sender) + '!\r\n', 'utf-8'))
+        irc.send(bytes('PRIVMSG ' + BOT_IRC_CHANNEL + ' :Hi there, ' + str(sender) + '!\r\n'  ))
 
     if message[0] == '!' and sender == BOT_OWNER:
         messageCommand = message[1:].split()
         if messageCommand[0] == "Author":
-            irc.send(bytes('PRIVMSG ' + BOT_IRC_CHANNEL + ' :I was created by ' + BOT_OWNER + '\r\n', 'utf-8'))
+            irc.send(bytes('PRIVMSG ' + BOT_IRC_CHANNEL + ' :I was created by ' + BOT_OWNER + '\r\n'  ))
 
 
 def sendmessage(rcv, msg):
-    print(bytes('PRIVMSG ' + rcv + ' :' + msg + '\r\n', 'utf-8'))
-    irc.send(bytes('PRIVMSG ' + rcv + ' :' + msg + '\r\n', 'utf-8'))
+    print(bytes('PRIVMSG ' + rcv + ' :' + msg + '\r\n'  ))
+    irc.send(bytes('PRIVMSG ' + rcv + ' :' + msg + '\r\n'  ))
 
 
 irc = socket.socket()
 irc.connect((BOT_IRC_SERVER, BOT_IRC_PORT))
 irc.recv(4096)
-irc.send(bytes('NICK ' + BOT_NICKNAME + '\r\n', 'utf-8'))
+irc.send(bytes('NICK ' + BOT_NICKNAME + '\r\n'  ))
 pingChecker(irc.recv(4096))
-irc.send(bytes('USER xjBot xjBot xjBot : xj IRC\r\n', 'utf-8'))
+irc.send(bytes('USER xjBot xjBot xjBot : xj IRC\r\n'  ))
 pingChecker(irc.recv(4096))
-irc.send(bytes('JOIN ' + BOT_IRC_CHANNEL + '\r\n', 'utf-8'))
+irc.send(bytes('JOIN ' + BOT_IRC_CHANNEL + '\r\n'  ))
 
 while 1:
     line = irc.recv(4096)
     print(line)
     pingChecker(line)
-    if line.find(bytes('PRIVMSG', 'utf-8')) != -1 or line.find(bytes('NOTICE', 'utf-8')) != -1:
+    if line.find(bytes('PRIVMSG'  )) != -1 or line.find(bytes('NOTICE'  )) != -1:
         messagechecker(line)
